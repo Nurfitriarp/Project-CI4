f3521b6c3204a95e8b77a65ea1c27f4e2eafe6c2
Nurfitriarp <93233737+Nurfitriarp@users.noreply.github.com>
2025-07-08 10:05:10 +0700

    initial commit

---
+++ company-profile/app/Config/Routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+// route admin dashboard
+$routes->get('dashboard', 'DashboardController::index'); 
